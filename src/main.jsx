b6e0fd47fa085f5278341c5f160a1eae2e8e04c1
rocky222558800 <rocky222558800@gmail.com>
2026-03-23 02:03:27 +0530

    Add button to mark attendance in App component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Smart Attendance System</h1>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Smart Attendance System</h1>
+      <button onClick={() => alert("Marked Present!")}>
+        Mark Attendance
+      </button>
+    </div>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
